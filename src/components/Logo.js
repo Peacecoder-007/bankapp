ed9ec3c91f89007bc3cf5558d3748aa21bf92093
VishalVinayRam <vishalvinayram5432@gmail.com>
2022-04-20 13:48:52 +0530

    minor changes 2

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export const Logo = () => {
     return(
-        <h1 id="logo"><i className='bx bxs-paper-plane' ></i> Vishal BankApp</h1>
+        <h1 id="logo">Vishal BankApp</h1>
     )
 }
